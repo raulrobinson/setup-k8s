0f08772b40bc20425d48f82ebda6fe3c2431c0cb
rasysbox <71100949+raulrobinson@users.noreply.github.com>
2023-05-16 19:54:16 -0500

    locations-service

---
+++ locations-service/src/main/java/com/rasysbox/locationsservice/LocationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rasysbox.locationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationsServiceApplication.class, args);
+    }
+
+}
---
+++ locations-service/src/test/java/com/rasysbox/locationsservice/LocationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasysbox.locationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
